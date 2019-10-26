07e889d3ba9fa01cc9564abc09c0ffa6841c2133
user <you@exampalex.com>
2019-10-26 16:57:13 +0100

    touches and adds code to  CharacterList.vue

---
+++ src/components/CharacterList.vue
@@ -0,0 +1,26 @@
+<template lang="html">
+  <div class="character-list" v-if="characters.length">
+    <character-list-item
+    v-for="(character, index) in characters"
+    :character="character"
+    :key="index"
+    >
+  </character-list-item>
+
+  </div>
+
+</template>
+
+<script>
+import CharacterListItem from "@/components/CharacterListItem";
+export default {
+  name: "character-list",
+  props: ["characters"],
+  components: {
+    "character-list-item": CharacterListItem
+  }
+};
+</script>
+
+<style lang="css" scoped>
+</style>
