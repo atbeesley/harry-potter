f1f377acabccc7bf198332cced1bd69ebb38c669
user <you@exampalex.com>
2019-10-26 17:22:36 +0100

    adds code to CharacterListItem.vue

---
+++ src/components/CharacterListItem.vue
@@ -0,0 +1,28 @@
+<template lang="html">
+  <div class="characterlistitem">
+    <p>{{ character.name }}</p>
+      <button v-on:click="displayInfo">More Info &rarr;</button>
+
+  </div>
+
+</template>
+
+<script>
+import { eventBus } from '@/main.js';
+
+export default {
+  name: "character-list-item",
+  props: ["character"],
+  methods: {
+    displayInfo: function () {
+      eventBus.$emit("character-selected", this.character);
+    },
+    addFavourite: function () {
+      eventBus.$emit("favourite-added", this.character);
+    }
+  }
+};
+</script>
+
+<style lang="css" scoped>
+</style>
